924f9908c703e60f5dff503b479d37d8c8a07786
2106psyto <2106psyto@gmail.com>
2023-11-26 18:31:34 +0900

    Extracted core business logic to  module.

--- src/dop_group_sum_lesson.py
+++ src/dop_group_sum_lesson.py
@@ -1,21 +1,11 @@
 import argparse as ap
+import datetime
 import json
 import os
 from jinja2 import Environment, FileSystemLoader
 from jsonschema import validate, ValidationError
-import pydash as _
+from expence import aggregate_by_div
 
-
-def aggregate_by_div(divs):
-    aggregated = _.group_by(divs, ['div_code'])
-    sum_by_div = _.map_(aggregated,
-                lambda o, k:
-                    {
-                        'div_code': k,
-                        'actual': _.sum_by(o, ['actual']),
-                        'planned': _.sum_by(o, ['plan'])
-                    })
-    return sum_by_div
 
 def get_html_template():
     env = Environment(loader=FileSystemLoader('./templates'), autoescape = True)
@@ -33,15 +23,10 @@ if __name__ == '__main__':
     parser.add_argument('infile', help='Input JSON file')
     args = parser.parse_args()
 
-    with open(os.path.join(os.getcwd(),'src', 'input_data_schema.json')) as schema_file:
-        schema = json.load(schema_file)
-
     with open(args.infile) as w:
         jj = json.load(w)
 
     try:
-        validate(instance=jj, schema=schema)
-        import datetime
         now = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
         results = {'calcdate':now,'results':aggregate_by_div(jj)}
         template = get_html_template()
---
+++ src/expence.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import json
+import os
+from jsonschema import validate, ValidationError
+import pydash as _
+
+
+def aggregate_by_div(divs):
+    with open(os.path.join(os.getcwd(),'src', 'input_data_schema.json')) as schema_file:
+        schema = json.load(schema_file)
+        validate(instance=divs, schema=schema)
+
+    aggregated = _.group_by(divs, ['div_code'])
+    sum_by_div = _.map_(aggregated,
+                lambda o, k:
+                    {
+                        'div_code': k,
+                        'actual': _.sum_by(o, ['actual']),
+                        'planned': _.sum_by(o, ['plan'])
+                    })
+    return sum_by_div
